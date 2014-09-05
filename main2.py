f0151b33fc5450d9efe12ba4d93bc7fa04df6ff6
spandanb <spandan.777@gmail.com>
2014-09-05 06:22:40 -0400

    Completed methods that iterate over package and module list

--- main2.py
+++ main2.py
@@ -6,12 +6,28 @@ import argparse
 import pylint as pyl
 
 
+
+def create_bins(module):
+    """
+    Creates a dictionary of entity type to lists
+    
+    Arguments:
+        module:- a FileLeaf object representing a py module
+    
+    Returns: a dictionary of entity type to lists 
+    """
+
+    
+
 def create_file_tree(rootdir):
-    """Creates and returns k-tree that represents the dir hierarchy and
-    parent child relationships. Very similar to 
+    """Creates a k-tree that takes the dir hierarchy and
+    parent-child relationships and turns it into package-module
+    and package - sub-package relationship.
     
     Arguments:
         rootdir:- path to rootdir of project being analyzed.
+    
+    Returns: The file tree
     """
     
     if not os.path.isdir(rootdir):
@@ -22,21 +38,22 @@ def create_file_tree(rootdir):
     ptr =  None
     this = None
     for selfpath, dirs, files in os.walk(rootdir):
-        #this is own path
+        #selfpath is own path
         #dirs is child dirs
         #files is child files
         this = pathmap.get(selfpath, root)
         for d in dirs:
-            path = selfpath + '/' + d
+            path = ut.abspath(selfpath, d)
             ptr = this.add_child_dir(path)
             pathmap[path] = ptr
         
         for f in files:
-            path = selfpath + '/' + f
+            path = ut.abspath(selfpath, f)
             ptr = this.add_child_file(path)
             pathmap[path] = ptr
-    
-    print root
+            if f == "__init__.py":
+                this.set_package(True)
+    #print root
     return root
 
 def error(msg, errno=-1):
@@ -68,3 +85,7 @@ if __name__ == '__main__':
     #"/Users/spandan/Documents/pproj/stat/test_data/tst"
     
     proj_file_tree = create_file_tree(rootdir)
+    for package in proj_file_tree.package_list():
+        for module in package.module_list():
+            bins = create_bins(module)
+#            m = create_module_obj(bins)
--- utils.py
+++ utils.py
@@ -1,5 +1,4 @@
 
-
 class FileLeaf():
     """Represents a child file.     
     """
@@ -12,6 +11,7 @@ class FileLeaf():
     def get_path(self):
         return self.path
 
+#TODO: project/package object inherits from FileTree?
 class FileTree():
     """Represents a directory hierarchy with children files.     
     """
@@ -21,30 +21,95 @@ class FileTree():
         self.path = path
         self.child_files = []
         self.child_dirs = []
+        self.is_package = False
 
     def __str__(self):
-        dirs = map(str, self.child_dirs)
-        files = map(str, self.child_files)
-        return "{} --> Dirs: {}, Files: {}".format(self.path, dirs, files)
-    
+        #dirs = map(str, self.child_dirs)
+        #files = map(str, self.child_files)
+        #return "{} --> Dirs: {}, Files: {}".format(self.path, dirs, files)
+        return "{}".format(self.path)
+   
+    def __repr__(self):
+        return "{}".format(self.path)
+
+
     def add_child_file(self, child):
         """Adds a child file"""
         child = FileLeaf(child)
         self.child_files.append(child)
+        #print "self.path is {}, child is {}".format(self.path, child.path)
         return child
     
     def add_child_dir(self, child):
         """Adds a child directory"""
         child = FileTree(child)
         self.child_dirs.append(child)
+        #print "self.path is {}, child is {}".format(self.path, child.path)
         return child
 
     def get_path(self):
         return self.path
     
-    def traverse(self):
+    def set_package(self, is_package):
         """
+        Sets whether directory is package
+        
+        Arguments:-
+            is_package: (True|False)
         """
-        for d in dirs: 
-        pass
+        self.is_package = is_package
     
+    def package_list(self):
+        """Iterate over list of python packages"""
+        #TODO: turn into iterator?
+        packages = []
+        if self.is_package:
+            #Append package
+            packages.append(self)
+        for d in self.child_dirs:
+            #Append child sub-packages
+            child_package_list = d.package_list()
+            if child_package_list: 
+                packages += child_package_list
+        return packages
+    
+    def module_list(self):
+        """Iterate over list of python modules in this package"""
+        #TODO: turn into iterator?
+        if not self.is_package:
+            return None
+        else:
+            modules = []
+            for f in self.child_files: 
+                if is_module(f):
+                    modules.append(f)
+            return modules
+
+    
+def abspath(parent, child):
+    """ 
+    Calculates the absolute path given the parent dir 
+    and child
+    
+    Argument:-
+        parent: path to the parent dir 
+        child: sub-dir or file
+    
+    Returns: the absolute path
+    """
+    if parent[-1] == "/":
+        return parent + child
+    else:
+        return parent + "/" + child
+
+def is_module(fileleaf):
+    """
+    Determines whether a FileLeaf obj represents a 
+    python module
+    
+    Arguments:-
+        fileleaf: a FileLeaf object
+    
+    Returns: (True| False)
+    """
+    return fileleaf.path[-3:] == ".py"
